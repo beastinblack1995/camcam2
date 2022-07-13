5437e1d3d05c91aeab9e4fccbc1f0f6630045694
beastinblack1995 <109196514+beastinblack1995@users.noreply.github.com>
2022-07-13 10:18:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,98 @@
+import cv2
+from sys import stdout
+from makeup_artist import Makeup_artist
+import logging
+from flask import Flask, render_template, Response, request, jsonify
+from flask_socketio import SocketIO
+from camera import Camera
+import binascii
+from utils import base64_to_pil_image, pil_image_to_base64
+import pickle
+import numpy as np
+import cv2
+# import jsonify
+import base64
+from PIL import Image
+import cv2
+from io import StringIO
+from io import BytesIO
+import numpy as np
+import numpy as np
+from tensorflow.keras.models import model_from_json
+from flask import Flask, render_template, Response
+#----------------- Video Transmission ------------------------------#
+app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(stdout))
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+camera = Camera(Makeup_artist())
+
+
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+# load json and create model
+json_file = open('model/emotion_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+emotion_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+emotion_model.load_weights("model/emotion_model.h5")
+# start the webcam feed
+
+@socketio.on('input image', namespace='/test')
+def test_message(input):
+    input = input.split(",")[1]
+    camera.enqueue_input(input)
+    #camera.enqueue_input(base64_to_pil_image(input))
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    app.logger.info("client connected")
+
+@app.route('/', methods=["POST",'GET'])
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+def gen():
+    while True:
+        # Find haar cascade to draw bounding box around face
+        frame = camera.get_frame()
+        frame = readb64(frame)
+        frame = cv2.resize(frame, (640,480))
+        if not ret:
+            break
+        face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # detect faces available on camera
+        num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+
+        # take each face available on the camera and Preprocess it
+        for (x, y, w, h) in num_faces:
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
+            roi_gray_frame = gray_frame[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)/255
+
+            # predict the emotions
+            emotion_prediction = emotion_model.predict(cropped_img)
+            maxindex = int(np.argmax(emotion_prediction))
+            cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+        #cv2.imshow('Emotion Detection', frame)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+
+if __name__ == '__main__':
+    socketio.run(app)
